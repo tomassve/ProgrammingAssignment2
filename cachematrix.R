e4c5b2e157e1ccb72f3372a338756681bdcf07d8
tomassve <tomas.g.svensson@gmail.com>
2014-05-25 16:43:40 +0200

    Improved comments

--- cachematrix.R
+++ cachematrix.R
@@ -14,28 +14,39 @@
 #   $ setinv  Set inverse matrix of the object
 #   $ getinv  Get inverse matrix
 makeCacheMatrix <- function(x = matrix()) {
+  
   cachedInv <- NULL
+  
+  #   $set()     Set matrix of the object
   set <- function(y) {
     x <<- y
     cachedInv <<- NULL
   }
+  
+  #   $get()     Get matrix
   get <- function() x
+  
+  #   $setinv()  Set inverse matrix of the object
   setinv <- function(inv) cachedInv <<- inv
+  
+  #   $getinv()  Get inverse matrix
   getinv <- function() cachedInv
+  
   list(set = set, get = get,
        setinv = setinv,
        getinv = getinv)
 }
 
 
-# cacheSolve(x) returns a matrix inverse of the special matrix object x
+# cacheSolve(x) returns a inverse matrix of the special matrix object x
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # Probe for cached version
   cachedInv <- x$getinv()
   if(!is.null(cachedInv)) {
     message("getting cached data")
     return(cachedInv)
   }
+  # No cached version. Get matrix and compute inverse
   data <- x$get()
   cachedInv <- solve(data, ...)
   x$setinv(cachedInv)
