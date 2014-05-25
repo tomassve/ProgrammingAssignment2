7707a6846bb6f5e6afbffa85c53d1bbd136cf53a
tomassve <tomas.g.svensson@gmail.com>
2014-05-25 15:34:30 +0200

    Committing programming assignment 2 in Coursera R Programming

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
+# The function cacheSolve(x) returns a matrix inverse of the special matrix
+# object x. If cacheSolve is repeatedly called with the same special matrix 
+# object a cached version of the inverse matrix is returned and thus costly
+# computations are saved.
+#
+# The special matrix object x is created by calling makeCacheMatrix(m), with
+# a numeric or complex matrix.
+#
 
-## Write a short comment describing this function
 
+# makeCacheMatrix returns a special "matrix" object as a list of functions:
+#   $ set     Set matrix of the object
+#   $ get     Get matrix
+#   $ setinv  Set inverse matrix of the object
+#   $ getinv  Get inverse matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  cachedInv <- NULL
+  set <- function(y) {
+    x <<- y
+    cachedInv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) cachedInv <<- inv
+  getinv <- function() cachedInv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+# cacheSolve(x) returns a matrix inverse of the special matrix object x
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  cachedInv <- x$getinv()
+  if(!is.null(cachedInv)) {
+    message("getting cached data")
+    return(cachedInv)
+  }
+  data <- x$get()
+  cachedInv <- solve(data, ...)
+  x$setinv(cachedInv)
+  cachedInv
+}
+
+# testCacheSolve test the code and returns an identity matrix of order 2
+testCacheSolve <- function() {
+  
+  # Create matrix object
+  m1<-makeCacheMatrix(matrix(c(2,1,1,2),2,2))
+  
+  # Calculate inverse matrix
+  invm1<-cacheSolve(m1)
+  
+  # Access cached inverse matrix
+  m1$get() %*% cacheSolve(m1)
+  
 }
